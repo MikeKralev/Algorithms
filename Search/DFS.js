0cfd7d06e3e9d3d9ec936fdb51bed25b14c3bfa0
Mike <110359429+MikeKralev@users.noreply.github.com>
2026-02-24 19:50:53 +0100

    add DFS InOrder PostOrder implementation

--- Search/DFS.js
+++ Search/DFS.js
@@ -11,3 +11,31 @@ function DFSPreOrder(tree) {
   helper(tree.root);
   return visited;
 }
+
+function DFSInOrder(tree) {
+  if (!tree.root) return [];
+  const visited = [];
+
+  function traverse(node) {
+    if (node.left) traverse(node.left);
+    visited.push(node.value);
+    if (node.right) traverse(node.right);
+  }
+
+  helper(tree.root);
+  return visited;
+}
+
+function DFSPostOrder(tree) {
+  if (!tree.root) return [];
+  const visited = [];
+
+  function traverse(node) {
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+    visited.push(node.value);
+  }
+
+  helper(tree.root);
+  return visited;
+}
